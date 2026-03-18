b8788399bc5ba8d2ff0d2e3ea910d501db944929
Norcane82 <83983805+Norcane82@users.noreply.github.com>
2026-03-18 13:06:38 +0100

    Delete whole rendered PDF groups from playlist

--- delete_slide.php
+++ delete_slide.php
@@ -9,21 +9,73 @@ $playlistData = playlist_load_normalized();
 $slides = $playlistData['slides'] ?? [];
 $newSlides = [];
 
+$targetSlide = null;
 foreach ($slides as $item) {
     if ((string)($item['id'] ?? '') === $id) {
-        $file = (string)($item['file'] ?? '');
+        $targetSlide = $item;
+        break;
+    }
+}
 
+if ($targetSlide === null) {
+    header('Location: admin.php');
+    exit;
+}
+
+$isPdfRenderedImage = (($targetSlide['type'] ?? '') === 'image') && (($targetSlide['sourceType'] ?? '') === 'pdf');
+$pdfSourceFile = $isPdfRenderedImage ? (string)($targetSlide['sourceFile'] ?? '') : '';
+$pdfRenderedFiles = [];
+$pdfOriginalFile = '';
+
+if ($isPdfRenderedImage) {
+    $pdfOriginalFile = $pdfSourceFile;
+
+    foreach ($slides as $item) {
+        if ((($item['type'] ?? '') === 'image') && (($item['sourceType'] ?? '') === 'pdf') && ((string)($item['sourceFile'] ?? '') === $pdfSourceFile)) {
+            $file = (string)($item['file'] ?? '');
+            if ($file !== '') {
+                $pdfRenderedFiles[] = $file;
+            }
+            continue;
+        }
+
+        $newSlides[] = $item;
+    }
+} else {
+    foreach ($slides as $item) {
+        if ((string)($item['id'] ?? '') === $id) {
+            $file = (string)($item['file'] ?? '');
+
+            if ($file !== '' && str_starts_with($file, 'uploads/')) {
+                $fullPath = __DIR__ . '/' . $file;
+                if (is_file($fullPath)) {
+                    @unlink($fullPath);
+                }
+            }
+
+            continue;
+        }
+
+        $newSlides[] = $item;
+    }
+}
+
+if ($isPdfRenderedImage) {
+    foreach ($pdfRenderedFiles as $file) {
         if ($file !== '' && str_starts_with($file, 'uploads/')) {
             $fullPath = __DIR__ . '/' . $file;
             if (is_file($fullPath)) {
                 @unlink($fullPath);
             }
         }
-
-        continue;
     }
 
-    $newSlides[] = $item;
+    if ($pdfOriginalFile !== '' && str_starts_with($pdfOriginalFile, 'uploads/')) {
+        $fullPath = __DIR__ . '/' . $pdfOriginalFile;
+        if (is_file($fullPath)) {
+            @unlink($fullPath);
+        }
+    }
 }
 
 foreach ($newSlides as $i => &$item) {
