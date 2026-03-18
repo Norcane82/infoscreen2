b7498dba4c41ee46b5544ddf3735a5cef7f923be
Norcane82 <83983805+Norcane82@users.noreply.github.com>
2026-03-18 11:14:52 +0100

    efactor slide delete action to v2 playlist logic

--- delete_slide.php
+++ delete_slide.php
@@ -1,24 +1,38 @@
 <?php
-require_once __DIR__ . '/functions.php';
+declare(strict_types=1);
 
-$id = (string)($_POST['id'] ?? '');
-$config = loadConfig();
-$playlist = loadPlaylist();
-$new = [];
+require_once __DIR__ . '/inc/bootstrap.php';
+require_once __DIR__ . '/inc/playlist.php';
 
-foreach ($playlist as $item) {
-    if (($item['id'] ?? '') === $id) {
-        if (!empty($item['file'])) {
-            $file = __DIR__ . '/uploads/' . basename((string)$item['file']);
-            if (is_file($file)) {
-                @unlink($file);
+$id = trim((string)($_POST['id'] ?? ''));
+$playlistData = playlist_load_normalized();
+$slides = $playlistData['slides'] ?? [];
+$newSlides = [];
+
+foreach ($slides as $item) {
+    if ((string)($item['id'] ?? '') === $id) {
+        $file = (string)($item['file'] ?? '');
+
+        if ($file !== '' && str_starts_with($file, 'uploads/')) {
+            $fullPath = __DIR__ . '/' . $file;
+
+            if (is_file($fullPath)) {
+                @unlink($fullPath);
             }
         }
+
         continue;
     }
-    $new[] = $item;
+
+    $newSlides[] = $item;
 }
 
-savePlaylist(normalizePlaylist($new, $config));
+foreach ($newSlides as $i => &$item) {
+    $item['sort'] = ($i + 1) * 10;
+}
+unset($item);
+
+playlist_save_normalized($newSlides);
+
 header('Location: admin.php');
 exit;
